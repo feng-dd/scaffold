a59ad9358ddd503f876047325aa0b65ae6de1dd0
tianhui.feng <fengdada2019@163.com>
2020-09-07 17:31:20 +0800

    创建项目

---
+++ src/main/java/com/feng/scaffold/GenerateProjectScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.feng.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateProjectScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateProjectScaffoldApplication.class, args);
+    }
+
+}
